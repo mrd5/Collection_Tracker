be00bd01c36a7294f5df76ca38a1b7e1160407f4
Mathew Dorish <mathewdorish@hotmail.com>
2019-01-03 20:48:58 -0600

    made minor changes to the navbar (such as not having the logout button visible while not logged in)

--- contact.php
+++ contact.php
@@ -1,8 +1,9 @@
 <?php
-
+session_start();
 if (isset($_POST["SUBMIT"])){
+
   $email = trim($_POST["email"]);
-  $message = trim($_POST["message"]) . "\n Sent by: " . $email;
+  $message = trim($_POST["message"]) . "\nSent by: " . $email;
   $headers = "From: " . $email . "\r\n";
 
   $to = "collectiontestemail@gmail.com";
@@ -25,9 +26,26 @@ if (isset($_POST["SUBMIT"])){
 <link rel="stylesheet" type="text/css" href="css/register.css" />
 <!-- //web font -->
 
-<script type="text/javascript" src="/javascript/login/login.js"></script>
+<script type="text/javascript" src="/javascript/contact/contact.js"></script>
 
 </head>
+<div class="topnav">
+  <?php
+  if (isset($_SESSION["username"])){
+    echo "<p>Welcome, " . $_SESSION["username"] . "</p>";
+  }
+  else{
+    echo "<a href='/login'>Login</a>";
+  }
+  ?>
+  <a href="/home">Home</a>
+  <a href="/contact" class="active-page">Contact</a>
+  <?php
+  if (isset($_SESSION["username"])){
+    echo "<a href='/logout'>Logout</a>";
+  }
+  ?>
+</div>
 <body>
   <!-- main -->
   <div class="main-w3layouts wrapper">
@@ -35,11 +53,6 @@ if (isset($_POST["SUBMIT"])){
     <div class="main-agileinfo">
       <div class="agileits-top">
         <form action="contact.php" method="post">
-          <label id="uname_msg" class="err_msg"></label>
-          <input type="text" name="email" placeholder="Email" id="email"/><br />
-          <label id="message_msg" class="err_msg"></label>
-          <textarea name="message" id=message" placeholder="Enter your message here..."></textarea>
-          <input type="submit" value="SUBMIT" name="SUBMIT" id="SUBMIT"/>
           <label id="label">
             <?php
             if (isset($_SESSION["error"])){
@@ -48,8 +61,23 @@ if (isset($_POST["SUBMIT"])){
             }
             ?>
           </label>
+          <label id="email_msg" class="err_msg"></label>
+          <?php 
+          if (isset($_SESSION["username"])){
+            $email = $_SESSION["email"];
+            echo "<input type='text' name='email' id='email' value='$email' /><br />";
+          }
+          else{
+            echo "<input type='text' name='email' placeholder='Email' id='email' /><br />";
+          }
+          ?>
+          <label id="message_msg" class="err_msg"></label>
+          <textarea name="message" id="message" placeholder="Enter your message here..."></textarea>
+          <p id="charColor"><span id="char_count">0</span> characters entered.</p>
+          <input type="submit" value="SUBMIT" name="SUBMIT" id="SUBMIT"/>
+          
         </form>
-        <script type="text/javascript" src="/javascript/login/login_r.js"></script>
+        <script type="text/javascript" src="/javascript/contact/contact_r.js"></script>
       </div>
     </div>
     <ul class="colorlib-bubbles">
--- home.php
+++ home.php
@@ -1,12 +1,69 @@
 <?php
 session_start();
-
-if (isset($_SESSION["username"])){
-	echo "Welcome, " . $_SESSION["username"] . "!";
-	echo "<a href='logout'>Click here to logout</a>"; 
-}
-else{
-	echo "You are not logged in.";
-}
-
+include('connection.php');
 ?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+
+<!-- web font -->
+<link rel="stylesheet" type="text/css" href="css/register.css" />
+<!-- //web font -->
+
+</head>
+<body>
+
+<div class="topnav">
+  <?php
+  if (isset($_SESSION["username"])){
+  	echo "<p>Welcome, " . $_SESSION["username"] . "</p>";
+  }
+  else{
+  	echo "<a href='/login'>Login</a>";
+  }
+  ?>
+  <a href="/home" class="active-page">Home</a>
+  <a href="/contact">Contact</a>
+  <?php
+  if (isset($_SESSION["username"])){
+  	echo "<a href='/logout'>Logout</a>";
+  }
+  ?>
+</div>
+	<!-- main -->
+	<div class="main-w3layouts wrapper">
+		<h1>Mathew's Collection Tracker</h1>
+		<?php
+		if (isset($_SESSION["username"])){
+			$username = $_SESSION["username"];
+			$getCollectionsQuery = "SELECT * FROM collections WHERE owner = '$username'";
+			$getCollections = $db->query($getCollectionsQuery);
+		}
+		else {
+			echo "Please login to use the website!";
+		}
+		?>
+		<ul class="colorlib-bubbles">
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+	<!-- //main -->
+</body>
+</html>
